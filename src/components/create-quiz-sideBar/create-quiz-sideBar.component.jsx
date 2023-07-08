60341bd1f29544688d476e8e8106a96b8a7f1bc8
Ahmed Sakr <80043395+Ahmed-Mohmed-Sakr@users.noreply.github.com>
2023-07-08 06:12:11 +0300

    add togle button to create quiz page to make design responsive with diffrant screens

--- src/components/create-quiz-sideBar/create-quiz-sideBar.component.jsx
+++ src/components/create-quiz-sideBar/create-quiz-sideBar.component.jsx
@@ -1,4 +1,6 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect, Fragment } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 import QuizSideBarItem from "../quiz-sidebar-item/quiz-sidebar-item.component";
 import { CreateQuizContext } from "../../context/create-quiz.context";
@@ -7,26 +9,58 @@ import "./create-quiz-sideBar.component.css";
 
 const CreateQuizSideBar = ({ handleAddQution }) => {
   const { questions } = useContext(CreateQuizContext);
-  return (
-    <div className="create-quiz-sidebar">
-      {questions.map((ques) => {
-        return (
-          <QuizSideBarItem
-            key={ques.id}
-            myquestion={ques}
-            handleAddQution={handleAddQution}
-          />
-        );
-      })}
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-      <button
-        type="button"
-        onClick={handleAddQution}
-        className="add-new-card-button"
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+  const handleResize = () => {
+    setIsSmallScreen(window.innerWidth <= 1050);
+  };
+  useEffect(() => {
+    setIsSmallScreen(window.innerWidth <= 1050);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <Fragment>
+      <div
+        className={`create-quiz-sidebar-container ${
+          showSidebar ? "show" : ""
+        } ${isSmallScreen ? "absolute" : ""}`}
       >
-        Add Question
+        <div className="create-quiz-sidebar">
+          {questions.map((ques) => {
+            return (
+              <QuizSideBarItem
+                key={ques.id}
+                myquestion={ques}
+                handleAddQution={handleAddQution}
+              />
+            );
+          })}
+
+          <button
+            type="button"
+            onClick={handleAddQution}
+            className="add-new-card-button"
+          >
+            Add Question
+          </button>
+        </div>
+      </div>
+      <button className="sidebar-toggle-button" onClick={toggleSidebar}>
+        <i>
+          <FontAwesomeIcon icon={faBars} className="menu-icon" />
+        </i>
       </button>
-    </div>
+    </Fragment>
   );
 };
 
