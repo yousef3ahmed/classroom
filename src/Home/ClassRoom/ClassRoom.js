10e08c40468304af4dd7343df30ceb95a3582366
yousef <yousef.amer@bld.ai>
2023-07-14 22:34:36 +0300

    fix the some bugs

--- src/Home/ClassRoom/ClassRoom.js
+++ src/Home/ClassRoom/ClassRoom.js
@@ -3,22 +3,33 @@ import App from "../../components/AppBar";
 import styles from "./ClassRoom.module.css";
 import Header from "../Header/Header";
 import FetchClassRoom from "./FetchClassRoom/FetchClassRoom";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
-function AdminHomeScreen(  ) {
+function AdminHomeScreen() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+
+      const token = localStorage.getItem("token");
+      if (!token) {
+        navigate("/");
+      }
+   
+  }, []);
+
+
   return (
-    < div className={styles.container} >
-      <App />   
+    <div className={styles.container}>
+      <App />
       <div className={styles.body}>
         <section className={styles.container2}>
           <Header />
         </section>
-
         <section className={styles.container2}>
-        <FetchClassRoom/>
+          <FetchClassRoom />
         </section>
-       
       </div>
-      
     </div>
   );
 }
--- src/Quiz Page/Quiz.js
+++ src/Quiz Page/Quiz.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import App from "../components/AppBar";
 import styles from "./Quiz.module.css";
 import Header from "./Header/Header";
 import FetchQuiz from "./FetchQuiz/FetchQuiz";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+
+
 
 function Quiz(  ) {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/");
+    }
+  }, []);
+
   return (
     < div className={styles.container} >
       <App />   
